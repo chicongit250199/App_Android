540bb0ed3a336de37e3ee7ac9a9e9661e60b6690
ThanhNguyen <thanh.nguyen@htklabs.com>
2016-03-29 12:10:18 +0700

    Create Domain Activity

--- app/src/main/java/antbuddy/htk/com/antbuddy2016/activities/login/LoginActivity.java
+++ app/src/main/java/antbuddy/htk/com/antbuddy2016/activities/login/LoginActivity.java
@@ -1,8 +1,12 @@
 package antbuddy.htk.com.antbuddy2016.activities.login;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import antbuddy.htk.com.antbuddy2016.activities.R;
 
@@ -10,10 +14,23 @@ import antbuddy.htk.com.antbuddy2016.activities.R;
  * Created by thanhnguyen on 28/03/2016.
  */
 public class LoginActivity extends Activity {
+
+    private Button accept_login_Button;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_login);
+
+        accept_login_Button = (Button) findViewById(R.id.accept_login_Button);
+        accept_login_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("DaiThanh", "Click here!");
+                Intent myIntent = new Intent(LoginActivity.this, DomainActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
+
 }
