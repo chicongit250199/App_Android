836adce9b00a870747bbcb931867cb4f496cbb3c
ThanhNguyen <thanh.nguyen@htklabs.com>
2016-04-06 18:21:14 +0700

    Show name, avatart at Profile

--- app/src/main/java/antbuddy/htk/com/antbuddy2016/modules/center/activities/ProfileFragment.java
+++ app/src/main/java/antbuddy/htk/com/antbuddy2016/modules/center/activities/ProfileFragment.java
@@ -10,7 +10,11 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+
 import antbuddy.htk.com.antbuddy2016.R;
+import antbuddy.htk.com.antbuddy2016.model.ObjectManager;
+import jp.wasabeef.glide.transformations.CropCircleTransformation;
 
 /**
  * Created by thanhnguyen on 30/03/2016.
@@ -25,7 +29,15 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
         imgAvatar = (ImageView) rootView.findViewById(R.id.imgAvatar);
+        Glide.with(getContext())
+                .load(ObjectManager.getInstance().getUserMe().getAvatar())
+                .override(100, 100)
+                .bitmapTransform(new CropCircleTransformation(getContext()))
+                .placeholder(R.drawable.ic_avatar_defaul)
+                .error(R.drawable.ic_avatar_defaul)
+                .into(imgAvatar);
         tv_user_name = (TextView) rootView.findViewById(R.id.tv_user_name);
+        tv_user_name.setText(ObjectManager.getInstance().getUserMe().getUsername());
         ll_user = (LinearLayout) rootView.findViewById(R.id.ll_user);
         ll_user.setOnClickListener(this);
         ll_user = (LinearLayout) rootView.findViewById(R.id.ll_swap);
